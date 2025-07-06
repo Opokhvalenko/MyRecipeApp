14ae6903d8c7249ba8320e416fd49499617797a0
OksanaP <oksanapokhvalenko@gmail.com>
2025-07-06 16:39:14 +0200

    Add web dependencies for Expo export

--- recipe-app-frontend/utils/api.js
+++ recipe-app-frontend/utils/api.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 import { Platform } from 'react-native';
 
-const API_BASE_URL = Platform.OS === 'android' ? 'http://192.168.1.100:5001/api' : 'http://localhost:5001/api';
+const RENDER_BACKEND_URL = 'https://myrecipeapp-489g.onrender.com/api';
+
+
+const LOCAL_BACKEND_URL = Platform.OS === 'android' ? 'http://192.168.1.100:5001/api' : 'http://localhost:5001/api';
+
+
+const API_BASE_URL = process.env.NODE_ENV === 'production' || Platform.OS === 'web'
+  ? RENDER_BACKEND_URL
+  : LOCAL_BACKEND_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
